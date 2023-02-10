85062b05f8ddcd40e410203dce374245c06ae80f
Dmitri Orlov <dmitri.orlov.anatolevich@gmail.com>
2023-02-10 21:07:02 +0600

    section pricing

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@ $(function () {
   
   var mixer = mixitup('.portfolio__content');
 
+  
+
 
   
+});
+
+const swiper = new Swiper('.swiper', {
+
+  pagination: {
+    el: '.castom-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: 'true',
 });
